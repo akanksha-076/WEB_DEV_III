0dd2700a1eaf4483324ed3b6039f18ab01e42d7b
Akanksha <a31695132@gmail.com>
2026-09-08 20:10:40 +0530

    MONDAY CLASS WORK

---
+++ index3.js
@@ -0,0 +1,15 @@
+// const express =require("express")
+// npm install morgan
+// const morgan =require("morgan")
+//app.use(express.json())
+//app.use(express.urlencoded({extended:true}))
+// app.use(morgan("dev"))
+//app.use(morgan("combined"))
+
+//app.get("/",(req,res)=>{
+    // res.send("HOME PAGE")
+    // })
+
+// models k aandr data.js file
+// create a json notes with key id title and description we will use it in another file so export this file 
+//go to controller create a file nodeController.js  create a functoin name get notes and expport it
